100cc9b893e9f9570f606fc3f6ec719960867f71
ariel <ariel@gmail.com>
2024-09-07 22:02:02 -0100

    klk

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,5 +2,8 @@
 def pow(a, b):
     result = 1
     for _ in range(b):
-        result *= a
+        if (b > 0):
+            result *= a
+        elif (b < 0)
+            result /= a
     return result
