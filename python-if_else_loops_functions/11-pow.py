a149f8ab77594aa89c1940cdbfbe39fcf56ac975
ariel <ariel@gmail.com>
2024-09-07 22:04:28 -0100

    asda

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if b < 0:
+        a = 1 / a
+        b = -b
+
     result = 1
     for _ in range(b):
-        if (b > 0):
-            result *= a
-        elif (b < 0):
-            result /= a
+        result *= a
     return result
