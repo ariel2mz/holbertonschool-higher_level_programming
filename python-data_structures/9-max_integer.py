d01e5eaa430ee1f856a85a9d7f0c05e73dc9bc3f
ariel <ariel@gmail.com>
2024-09-10 17:39:16 -0100

    asda

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+def max_integer(my_list=[]):
+    a = len(my_list)
+    if a == 0:
+        return None
+    else:
+        b = my_list[0]
+        for i in range(0, a):
+            if my_list[i] > b:
+                b = my_list[i]
+    return b
