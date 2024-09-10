fed68b083dd93eb34feb7058b5dbfe02a9872852
ariel <ariel@gmail.com>
2024-09-10 18:04:22 -0100

    klkl

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     a = len(my_list)
-    j = 0
     x = []
     if idx >= a:
         return my_list
     for i in range(0, a):
         if (i != idx):
-            x[j] = my_list[i]
-            j = j + 1
+            x.append(my_list[i])
     return x
