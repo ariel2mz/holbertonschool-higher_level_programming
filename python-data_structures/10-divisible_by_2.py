dea3c8f00f02ec98e8c6d2d4081b99cba6f536eb
ariel <ariel@gmail.com>
2024-09-10 17:45:32 -0100

    sadsa

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    a = len(my_list[])
+    b = my_list.copy()
+    for i in range(0, a):
+        if (my_list[i] % 2 == 0)
+            b[i] = 1
+        else
+            b[i] = 0
+    return b
