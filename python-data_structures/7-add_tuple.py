0c65c2f437e7f680a7676cf1094f8b29c167f2ee
ariel <ariel@gmail.com>
2024-09-10 17:18:02 -0100

    saa

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a(0) + tuple_b(0)
-    b = tuple_b(1) + tuple_a(1)
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_b[1] + tuple_a[1]
     nuevo = (a, b)
     return nuevo
