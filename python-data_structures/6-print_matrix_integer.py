10b4062ae2a1061d3234c93be1d636d111961438
ariel <ariel@gmail.com>
2024-09-10 15:38:36 -0100

    klk

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, 3):
+        print("")
         for j in range (0, 3):
             print("{:d}".format(matrix[i][j]))
