0018c6c95c1a6a4fb8e88311a367b7fe6a94cf29
ariel <ariel@gmail.com>
2024-09-10 17:46:07 -0100

    sadas

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    a = len(my_list[])
+    a = len(my_list)
     b = my_list.copy()
     for i in range(0, a):
         if (my_list[i] % 2 == 0)
