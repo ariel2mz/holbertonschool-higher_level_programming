7508385e7f3fe5b7d4723fb7a6a7f11bd326305a
ariel <ariel@gmail.com>
2024-09-10 15:52:23 -0100

    sadsa

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0, 3):
         for j in range (0, 3):
-            print("{:d} ".format(matrix[i][j]), end="")
+            if (j == 2):
+                print("{:d}".format(matrix[i][j]), end="")
+            else:
+                print("{:d} ".format(matrix[i][j]), end="")
         print("\n", end="")
