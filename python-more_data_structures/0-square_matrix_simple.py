e5c9de651f2dd956f0f3db170743fad5e7dac6df
ariel <ariel@gmail.com>
2024-09-11 13:37:02 -0100

    dsadsadsa

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix2 = matrix.copy()
+    matrix2 = []
     for i in range(0, len(matrix)):
+        matrix2.append([])
         for j in range(0, len(matrix)):
-            matrix2[i][j] = matrix[i][j] * matrix[i][j]
+            matrix2[i].append(matrix[i][j] * matrix[i][j])
     return matrix2
