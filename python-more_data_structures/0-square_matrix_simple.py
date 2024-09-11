cf32ca5fcf0beb23e45df131a9c2497f5c7a2687
ariel <ariel@gmail.com>
2024-09-11 13:38:14 -0100

    sdadas

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     matrix2 = []
     for i in range(0, len(matrix)):
         matrix2.append([])
-        for j in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
             matrix2[i].append(matrix[i][j] * matrix[i][j])
     return matrix2
