8bf85cbb747ff4725570448e8274cedbdbfa19ed
ariel <ariel@gmail.com>
2024-09-13 17:31:26 -0100

    sada

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     set_3 = []
-    for i in (set_2):
-        if i not in set_1:
+    for i in (set_1):
+        if i not in set_2:
             set_3.append(i)
-    for x in (set_1):
-        if x not in set_2:
-            set_3.append(i)
+    for x in (set_2):
+        if x not in set_1:
+            set_3.append(x)
     return set_3
+
