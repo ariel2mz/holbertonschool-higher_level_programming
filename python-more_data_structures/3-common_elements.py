7671ae4f94668fc316e3b69000d94866b0c64372
ariel <ariel@gmail.com>
2024-09-13 17:19:33 -0100

    jlkl

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@ def common_elements(set_1, set_2):
     set_3 = []
     for i in (set_1):
         if i in set_2:
-            set_3.append(set_1[i])
+            set_3.append(i)
     return set_3
             
                 
