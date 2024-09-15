45dc2aad5c5622fab9c39e8adfc259f72e273e75
ariel <ariel@gmail.com>
2024-09-15 20:14:28 -0100

    asdas

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    cantidad = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            cantidad = cantidad + 1
+    except IndexError:
+        pass
+    print()
+    return cantidad
