459dd40fa6d7cb4ad135d09d512eb3545e674882
ariel <ariel@gmail.com>
2024-09-18 13:48:21 -0100

    sadas

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides a simple definition for a Square class.
+
+The Square class is currently empty, with no attributes or methods. It serves as a
+basic structure for future development related to square functionality.
+"""
 class Square:
     """
     Class that defines a square.
