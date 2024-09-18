e183bcc250121dec39569f478ec781e8c815bc8a
ariel <ariel@gmail.com>
2024-09-18 14:27:02 -0100

    adsa

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,7 +7,7 @@ This module defines a Square class.
 class Square:
     """
 Class that defines a square with a private instance attribute 'size'.
-    
+
     Attributes:
         __size (int): The size of the square.
     """
@@ -15,7 +15,7 @@ Class that defines a square with a private instance attribute 'size'.
     def __init__(self, size):
         """
         Initializes the square with a given size.
-    
+
         Args:
             size (int): The size of the square.
         """
