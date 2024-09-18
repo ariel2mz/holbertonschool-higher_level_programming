052f957c921ef7193a77533c2021ac90d676b46f
ariel <ariel@gmail.com>
2024-09-18 14:10:35 -0100

    kklkl

---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides a simple definition for a Square class.
+
+The Square , with no attributes or methods. It serves as a
+basic strucrelated to square functionality.
+"""
+
+
+class Square:
+    """
+    Class that defines a square.
+
+    This class is cunot have any attributes or methods.
+    It serves aor future implementation of square-related functionality.
+
+    Attributes:
+        None
+    Methods:
+        None
+    """
+
+    size = None
+    pass
