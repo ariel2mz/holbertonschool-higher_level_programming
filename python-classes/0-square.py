b9f99a41a22bb91ecbef98314fbc2e4f4b2b30b0
ariel <ariel@gmail.com>
2024-09-18 13:43:03 -0100

    sadsa

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Class that defines a square.
+
+    This class is currently empty and does not have any attributes or methods.
+    It serves as a basic structure for future implementation of square-related functionality.
+
+    Attributes:
+        None
+    Methods:
+        None
+    """
+    pass
