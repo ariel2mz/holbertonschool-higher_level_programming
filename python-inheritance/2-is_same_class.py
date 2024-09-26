559e64936b21b8e9cd62f26bbcb7f73d79bb1bd2
ariel <ariel@gmail.com>
2024-09-26 13:12:08 -0300

    sadsa

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -12,4 +12,7 @@ def is_same_class(obj, a_class):
         AHODSHDOSADS
         ASHODHSAO
         """
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
