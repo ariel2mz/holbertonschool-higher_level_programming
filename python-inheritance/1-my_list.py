011f6b064742fbdfb7c02f27962aca04f3978cf1
ariel <ariel@gmail.com>
2024-09-26 12:22:39 -0300

    saa

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module provides a simple definition for a Rectangle class.
+
+The Rectangle , with no attributes or methods. It serves as a
+basic strucrelated to square functionality.
+"""
+
+
 class MyList(list):
     """
     HOLA HOLA HOLA HOLA 
