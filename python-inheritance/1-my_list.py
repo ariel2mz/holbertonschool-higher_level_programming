8fc60b0d40c27bdb3b60bb41a77fca14bd9c31d9
ariel <ariel@gmail.com>
2024-09-26 12:21:28 -0300

    asda

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class MyList(list):
+    """
+    HOLA HOLA HOLA HOLA 
+    """
+
+
     def print_sorted(self):
+        """
+        AHODSHDOSADS
+        ASHODHSAO
+        """
         # Create a sorted copy of the list and print it
         sorted_list = sorted(self)
         print(sorted_list)
