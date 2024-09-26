945aa449bd73f34faba130bc3fd9d4ac8746e639
ariel <ariel@gmail.com>
2024-09-26 13:09:45 -0300

    asda

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a simple definition for a Rectangle class.
+
+The Rectangle , with no attributes or methods. It serves as a
+basic strucrelated to square functionality.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        AHODSHDOSADS
+        ASHODHSAO
+        """
+    return isinstance(obj, a_class)
