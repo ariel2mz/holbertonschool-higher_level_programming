c8ad11263f27e40727debc2fe2eee8f106c8d6cf
ariel <ariel@gmail.com>
2024-09-26 13:46:24 -0300

    asdsa

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This module provides a simple definition for a Rectangle class.
+This module provides a simple definition for a BaseGeometry class.
 
 The Rectangle , with no attributes or methods. It serves as a
 basic strucrelated to square functionality.
 """
 
 
-class Rectangle:
+class BaseGeometry:
     """
 Class that defines a square with a private instance attribute 'size'.
 """
