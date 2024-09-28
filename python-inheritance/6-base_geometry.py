8aa378312c549eb1dad58a9e39a538a53d34093d
ariel <ariel@gmail.com>
2024-09-27 22:52:17 -0300

    dsa

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,9 +4,10 @@ This module provides a simple definition for a BaseGeometry class.
 
 """
 
+
 class BaseGeometry:
     """BaseGeometry class."""
-    
+
     def area(self):
         """Calculate area. Raises Exception if not implemented."""
         raise Exception("area() is not implemented")
