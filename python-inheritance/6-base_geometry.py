1a4f5cd31452d1622fcc9ec80437ccc91bb4813f
ariel <ariel@gmail.com>
2024-09-27 22:44:36 -0300

    klk

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a simple definition for a BaseGeometry class.
+
+The Rectangle , with no attributes or methods. It serves as a
+basic strucrelated to square functionality.
+"""
+
+
+class BaseGeometry:
+    """
+Class that defines a square with a private instance attribute 'size'.
+"""
+
+def area(self):
+    """
+    quesoquesoqueso
+    """
+    raise Exception("area() is not implemented")
