a5d47610c4d6d47c76d5a91af924aee2b76b8e61
ariel <ariel@gmail.com>
2024-09-28 13:02:04 -0300

    SADAS

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module provides a simple definition for a BaseGeometry class.
+This module provides a simple definition for a
+BaseGeometry class.
 
 """
 
@@ -8,11 +9,15 @@ This module provides a simple definition for a BaseGeometry class.
 class BaseGeometry:
     """BaseGeometry class."""
 
+
     def area(self):
-        """Calculate area. Raises Exception if not implemented."""
+        """Calculate area. Raises Exception
+        if not implemented."""
+
         raise Exception("area() is not implemented")
     def integer_validator(self, name, value):
         """ i dont fucking care """
+
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
