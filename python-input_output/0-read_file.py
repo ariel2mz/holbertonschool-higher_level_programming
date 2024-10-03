98d0f5a04aaf4aa57a7e5f0e36131baf4951f75b
ariel <ariel@gmail.com>
2024-10-03 11:33:28 -0300

    sadsa

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+QUESOOOO
+"""
 def read_file(filename=""):
+    """
+    :VVVVVV
+    """
     with open("filename.txt", "r") as file:
         content = file.read()
     print(content)
