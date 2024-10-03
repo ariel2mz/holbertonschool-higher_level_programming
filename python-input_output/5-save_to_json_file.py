a808efc689334766ae7b0b0ba2d339fbd5c2f831
ariel <ariel@gmail.com>
2024-10-03 12:57:06 -0300

    asdadsa

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+QUESOOOO
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w") as file:
+        return file.write(json.dumps(my_obj))
