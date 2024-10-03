4c71d8e82cf45ff9728b8a4b97dd06adb22b62c4
ariel <ariel@gmail.com>
2024-10-03 11:59:08 -0300

    asdsa

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,8 @@ adsda
 
 def append_write(filename="", text=""):
     """
-    Appends the given text to the specified file and returns the number of characters added.
+    Appends the given text to the specified file 
+    and returns the number of characters added.
 
     :param filename: The name of the file to append to.
     :param text: The text to be appended to the file.
