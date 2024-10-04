947416562a23741ab8a502740d4bb06d47b3fec9
ariel <ariel@gmail.com>
2024-10-04 14:55:59 -0300

    sadsa

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,8 +6,9 @@ sadsadsad
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structures 
-    (list, dictionary, string, integer, boolean) for JSON serialization of an object.
+    Returns the dictionary description with simple data structures
+    (list, dictionary, string, integer, boolean)
+    for JSON serialization of an object.
 
     :param obj: The instance of a class to be serialized.
     :return: A dictionary containing all serializable attributes of the object.
