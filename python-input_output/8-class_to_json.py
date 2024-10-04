21a461aecb9f5e9af0cb0ad092499b073aa7c93f
ariel <ariel@gmail.com>
2024-10-04 14:54:43 -0300

    jkjk

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+sadsadsad
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structures 
+    (list, dictionary, string, integer, boolean) for JSON serialization of an object.
+
+    :param obj: The instance of a class to be serialized.
+    :return: A dictionary containing all serializable attributes of the object.
+    """
+    return obj.__dict__
