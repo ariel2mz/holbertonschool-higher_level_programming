d57d0223e3ab95eeda24bd988dccab5f9518e614
ariel <ariel@gmail.com>
2024-10-04 15:05:19 -0300

    asdasdas

---
+++ python-input_output/9-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+sadsadsad
+"""
+
+
+class Student
+    """
+Class that defines a square with a private instance attribute 'size'.
+
+    Attributes:
+        first_name (str)
+        last_name (str)
+        age (int)
+
+    """
+    first_name (str)
+    last_name (str)
+    age (int)
+
+
+def to_json(self):
+    """
+    Returns the dictionary description with simple data structures
+    (list, dictionary, string, integer, boolean)
+    for JSON serialization of an object.
+
+    :param obj: The instance of a class to be serialized.
+    :return: A dictionary containing all serializable attributes of the object.
+    """
+    return self.__dict__
