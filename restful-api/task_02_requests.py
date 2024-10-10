cf867e2f10027c131a19cb56e01f85f15a8079dd
ariel <ariel@gmail.com>
2024-10-10 16:22:02 +0000

    jkjjkk

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,12 +3,9 @@
 adssada
 """
 
-r = requests.get('https://jsonplaceholder.typicode.com/')
-codigo = r.status_code
-printf(f"{codigo}")
 
 def fetch_and_print_posts():
-    r = requests.get('https://jsonplaceholder.typicode.com/')
+    r = r.get('https://jsonplaceholder.typicode.com/')
     codigo = r.status_code
     printf(f"{codigo}")
     if codigo == 200:
