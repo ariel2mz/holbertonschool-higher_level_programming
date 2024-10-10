5751afa7281ce750e70a03b91d07cc07f004a066
ariel <ariel@gmail.com>
2024-10-10 16:20:07 +0000

    sadsadsa

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,3 +6,12 @@ adssada
 r = requests.get('https://jsonplaceholder.typicode.com/')
 codigo = r.status_code
 printf(f"{codigo}")
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/')
+    codigo = r.status_code
+    printf(f"{codigo}")
+    if codigo == 200:
+        info = r.json()
+        for key, value in json_data.items():
+        print(f"{key}")
