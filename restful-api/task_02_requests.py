2160ff5d080fb36cb16e57a5d430920b88fb74e1
ariel <ariel@gmail.com>
2024-10-10 16:31:26 +0000

    asddsa

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -2,13 +2,28 @@
 """
 adssada
 """
-
+import requests
 
 def fetch_and_print_posts():
-    r = r.get('https://jsonplaceholder.typicode.com/')
+    r = requests.get('https://jsonplaceholder.typicode.com/')
     codigo = r.status_code
     printf(f"{codigo}")
     if codigo == 200:
         info = r.json()
         for key, value in json_data.items():
             print(f"{key}")
+
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/')
+    codigo = r.status_code
+    printf(f"{codigo}")
+    if codigo == 200:
+        lista = []
+        info = r.json()
+        for x in info:
+            post_dict = {
+            "id": post.get("id"),
+            "title": post.get("title"),
+            "body": post.get("body")
+        }
+        posts_list.append(post_dict)
