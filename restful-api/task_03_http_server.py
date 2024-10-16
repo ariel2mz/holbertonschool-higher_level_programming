6aa232f1a9d983e44c1084152712c0ddae2dee40
ariel <ariel@gmail.com>
2024-10-16 11:34:09 -0300

    klklkl

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,8 +5,19 @@ import json
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Hello, this is a simple API!")
         if self.path == "/data":
             data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_data.encode('utf-8'))
+        elif self.path == "/info":
+            data = {"version": "1.0", "description": "A simple API built with http.server"}
             json_data = json.dumps(data)
             self.send_response(200)
             self.send_header("Content-type", "application/json")
