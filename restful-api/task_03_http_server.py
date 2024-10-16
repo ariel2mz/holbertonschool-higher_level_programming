3cb5e6e71fe51dad759ce4400d33682b670a351d
ariel <ariel@gmail.com>
2024-10-16 11:21:45 -0300

    jkjkjk

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 import http.server
 import socketserver
+import json
+
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            json_data = json.dumps(data)
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json_data.encode('utf-8'))
 
 PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
