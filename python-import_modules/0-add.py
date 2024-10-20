7161203553ee5eddd8a1df5e31b5b50e13fe07c1
ariel <ariel@gmail.com>
2024-10-20 18:27:40 -0300

    sadsadsasadsa

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     from add_0 import add
 
-a = 1
-b = 2
-c = add(a,b)
-
-print("{} + {} = {}".format(a, b, c))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
