c51c1355540bb5f9f16c8e00f22b6de8e8a74fcd
ariel <ariel@gmail.com>
2024-10-31 15:52:12 +0000

    sadsadsa

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- sadadsadsada
-CREATE TABLE IF NOT EXISTS `force_name` (`id` INT NOT NULL DEFAULT 1, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `id_not_null` (`id` INT NOT NULL DEFAULT 1, `name` VARCHAR(256));
