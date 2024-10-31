42e1f02e69978ee03140843634f9c4f297aa2d26
ariel <ariel@gmail.com>
2024-10-31 15:24:52 +0000

    sadasdas

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- asdsadsadsadsadsadsa
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * to 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT * ON *.* to 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
