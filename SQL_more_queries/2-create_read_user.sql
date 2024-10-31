7c12dae549ecf3c1cebdf94346a9a6027166b057
ariel <ariel@gmail.com>
2024-10-31 15:36:39 +0000

    asdsasadsa

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- llklk
+CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0c_2.* TO `user_0d_2`@`localhost`;
+FLUSH PRIVILEGES;
+
