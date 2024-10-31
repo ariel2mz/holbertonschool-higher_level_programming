8ed5c8e2076c21ef0deed4eb01f221f4ed2c3e7c
ariel <ariel@gmail.com>
2024-10-31 16:34:13 +0000

    sadsadsadsa

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,12 @@
--- sadadsadsada
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the created database
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `states` (`id` INT NOT NULL DEFAULT AUTO_INCREMENT, `name` VARCHAR(256), UNIQUE (`id`), PRIMARY KEY (`id`));
+
+-- Create the states table if it doesn't exist
+CREATE TABLE IF NOT EXISTS `states` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
+);
