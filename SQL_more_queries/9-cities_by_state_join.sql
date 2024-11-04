117301f5f49be6a1130c7479be02c9199f3dcddd
ariel <ariel@gmail.com>
2024-11-04 02:35:05 +0000

    asdsadsad

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- asdsadsadsadsa
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
