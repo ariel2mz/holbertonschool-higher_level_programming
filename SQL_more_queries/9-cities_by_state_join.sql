8638c8b7b49363caa1c5b6a52e341153ff289093
ariel <ariel@gmail.com>
2024-11-04 02:31:02 +0000

    sadsadsa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- asdsadsadsadsa
 USE hbtn_0d_usa;
-SELECT cities.id, cities.name, 
-       (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-FROM cities
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
