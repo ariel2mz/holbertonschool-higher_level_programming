f9dc931e182dd96607175660e00a73aa4bb1b093
ariel <ariel@gmail.com>
2024-11-18 16:43:41 -0300

    asdasdsadsa

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,10 @@
+
+import json
+def serialize_and_save_to_file(data, filename):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    # Your code here to load and deserialize data from the specified file
+    with open(filename, "r") as file:
+        data = json.load(file)
