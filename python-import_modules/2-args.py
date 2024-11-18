38023cd8b27c1e65f93a0f576a843fcad8be34b8
ariel <ariel@gmail.com>
2024-11-18 13:12:46 -0300

    sadsadsa

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    argumentos = sys.argv
-    cantidad = len(argumentos) - 1
-
-    if cantidad == 0:
-        print("0 arguments.")
-    elif cantidad == 1:
-        print("1 argument:")
+    import sys
+    argv = sys.argv[1:]  # Exclude the script name
+    argc = len(argv)
+    if argc == 0:
+        print(f"{argc} arguments.")
     else:
-        print(f"{cantidad} arguments:")
-
-    for i in range(1, cantidad + 1):
-        print(f"{i}: {argumentos[i]}")
+        print(f"{argc} argument{'s' if argc > 1 else ''}:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
