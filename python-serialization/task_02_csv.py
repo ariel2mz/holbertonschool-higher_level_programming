c24e41dad048c214ebdd0b3be3d79127b1f38e3b
ariel <ariel@gmail.com>
2024-11-19 14:42:28 -0300

    dasdsa

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,7 +4,7 @@ import json
 def convert_csv_to_json(filename):
     
     try:
-        with open(filename, 'r') as file:
+        with open(filename, 'wr') as file:
             aux = csv.DictReader(file)
             json.dump('data.json', aux)
         return True
