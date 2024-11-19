c60fd220f6aadb26f02ef3196f59ecc6c9946af9
ariel <ariel@gmail.com>
2024-11-19 15:42:53 -0300

    asdsadsdsa

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -6,7 +6,8 @@ def serialize_to_xml(dictionary, filename):
 
     for key, value in dictionary.items():
         child = ET.SubElement(root, "child")
-        child.set(key, value)
+        child.set(key, key)
+        child.text = str(value)
     
     tree = ET.ElementTree(root)
     with open(filename, "wb") as file:
