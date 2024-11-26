6ff403fac6222b12120981bc7eb7659ccc81a903
ariel <ariel@gmail.com>
2024-11-26 16:25:17 -0300

    sadsadsadsa

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -39,6 +39,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
     @property
     def height(self):
         """
@@ -57,6 +58,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
     def area(self):
         """
         sadsadsadsa
