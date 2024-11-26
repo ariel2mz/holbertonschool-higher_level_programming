54bb9ef2d992bd40217bb98ac53a0e8027790e8d
ariel <ariel@gmail.com>
2024-11-26 16:58:09 -0300

    asdsadsa

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -13,6 +13,7 @@ class Rectangle:
     A class to represent a rectangle with width and height.
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Initialize the Rectangle object.
@@ -24,6 +25,7 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
     @property
     def width(self):
         """Get the width of the rectangle."""
