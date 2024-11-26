5b8f69e6c365cdf99f00d2fd918cff8fc2a00104
ariel <ariel@gmail.com>
2024-11-26 16:43:30 -0300

    jlklkllk

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -79,3 +79,10 @@ class Rectangle:
             str: A string that can recreate the rectangle using `eval`.
         """
         return f"Rectangle({self.__width}, {self.__height})"
+    def __del__(self):
+        """
+        Return adsadsadsa
+
+        asdsadsads
+        """
+        print("Bye rectangle...")
